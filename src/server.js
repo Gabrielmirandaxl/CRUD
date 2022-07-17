41a43bed2e4239fd4ba939dd9524414bb7acb544
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2022-07-17 13:29:08 -0300

    final structure

--- src/server.js
+++ src/server.js
@@ -1,10 +1,34 @@
 const express = require("express")
 const path = require("path")
 
+const router = require("./router")
+const mongodb = require("./database/connection")
+
 const app = express()
 
 
+//connection database
+mongodb.connect()
+
+//defineding folder static
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
+
+//defineding folder public
+app.use(express.static(path.join(__dirname, "public")))
+
+//receive data by method post
+app.use(express.urlencoded({ extended: true }))
+
+//router
+app.use("/", router)
+
+
+
+
+
 const port = process.env.port || 8080
-app.listen(port.at, () => {
+app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
